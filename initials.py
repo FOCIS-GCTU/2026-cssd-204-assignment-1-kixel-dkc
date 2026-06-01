e9b20bb609bd194e71426ef596a61a2de7eafa68
Chris Kouakou Diby <chriskixel@gmail.com>
2026-06-01 23:40:19 +0000

    Chris Kouakou Diby PY Assignment

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# file: initials .py
+# Descirption : print out my initials
+# Assignment Number 1
+#
+#Name:< CHRIS DIBY KOUAKOU
+#STUDENT ID<2425404544
+# Email: <2425404544@live.gctu.edu.gh
+# Grader: < AUGUSTUS >
+# On my < CHRIS DIBY KOUAKOU > This programming assignment is my own work
+# and i have not provided this code to any other student
+
+
 
 def main():
-  pass #code goes here
+   # Prints initials C, D, K in large block letters
+
+   print()
+   print("...CDK")
+   print()
 
 
-
-main()
+print()
+print(".......CCCCCCCC........DDDDDD..............kkk.......kk.....")
+print("....CCC................DDD...DDD...........kkk.....kk.......")
+print("...CCC.................DDD......DDD........kkk...kk.........")
+print("...CCC.................DDD......DDD........kkk..kk..........")
+print("...CCC.................DDD......DDD........kkkkk............")
+print("...CCC.................DDD......DDD........kkkkk............")
+print("...CCC.................DDD......DDD........kkk..kk..........")
+print("...CCC.................DDD......DDD........kkk...kk.........")
+print("....CCC...........**...DDD...DDD......**...kkk.....kk.....**")
+print("......CCCCCCCCC...**...DDDDDD.........**...kkk.......kk...**")
